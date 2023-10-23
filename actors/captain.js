97ede33d694abcaa67163f1bda25a38f3b18e8b0
mwcz <mwc@clayto.com>
2023-10-23 09:53:46 -0400

    collide poison balls with enemies

--- actors/captain.js
+++ actors/captain.js
@@ -1,242 +1,244 @@
 import Phaser from "phaser";
 import { Actor } from "./actor";
 import {
-  PIXEL_SCALE,
-  CAPTAIN_ATTACK_DAMAGE,
-  CAPTAIN_BASE_HP,
-  CAPTAIN_SPEED,
-  CAPTAIN_ATTACK_RANGE,
-  CAPTAIN_IDLE_AFTER_ATTACK,
+    PIXEL_SCALE,
+    CAPTAIN_ATTACK_DAMAGE,
+    CAPTAIN_BASE_HP,
+    CAPTAIN_SPEED,
+    CAPTAIN_ATTACK_RANGE,
+    CAPTAIN_IDLE_AFTER_ATTACK,
 } from "../variables";
 import { PoisonBall } from "./poison_ball";
 
 export class Captain extends Actor {
-  /** @param {Phaser.Scene} scene */
-  constructor(scene, config) {
-    super(scene, { hp: config.captainHP, damage: config.captainAttackDamage });
+    /** @param {Phaser.Scene} scene */
+    constructor(scene, config) {
+        super(scene, { hp: config.captainHP, damage: config.captainAttackDamage });
 
-    this.captainAttackRange = config.captainAttackRange;
-    this.captainIdleAfterAttack = config.captainIdleAfterAttack;
-    this.captainProjectileSpeed = config.captainProjectileSpeed;
-    this.captainSpeed = config.captainSpeed;
-  }
-  /** @param {Phaser.Scene} scene */
-  static preload(scene) {
-    scene.load.spritesheet("captain-idle", "images/captain_idle_strip.png", {
-      frameWidth: 64,
-      frameHeight: 64,
-    });
-    scene.load.spritesheet("captain-run", "images/captain_run_strip.png", {
-      frameWidth: 64,
-      frameHeight: 64,
-    });
-    scene.load.spritesheet(
-      "captain-attack",
-      "images/captain_attack_strip.png",
-      {
-        frameWidth: 64,
-        frameHeight: 64,
-      }
-    );
-    scene.load.spritesheet("poison-ball", "images/poisonBall_strip.png", {
-      frameWidth: 36,
-      frameHeight: 36,
-    });
-    scene.load.spritesheet(
-      "poison-ball-explosion",
-      "images/poisonBall_explosion_strip.png",
-      {
-        frameWidth: 96,
-        frameHeight: 96,
-      }
-    );
-    scene.load.audio("captain-dies", "audio/captain-dies.mp3");
-    scene.load.audio("captain-roar", "audio/captain-roar.mp3");
-    scene.load.audio("enemy-damaged", "audio/enemy-damaged.mp3");
-    scene.load.audio("enemy-hit-aah", "audio/enemy-hit-aah.mp3");
-  }
-  create() {
-    super.create();
-    this.captain = this.mainSprite = this.scene.add.sprite(250, 500);
-    this.captain.setScale(PIXEL_SCALE);
-    this.captain.play("captain-run");
-    this.scene.physics.add.existing(this.captain);
-    this.captain.setDataEnabled();
-    this.captain.data.set("actor", this);
+        this.captainAttackRange = config.captainAttackRange;
+        this.captainIdleAfterAttack = config.captainIdleAfterAttack;
+        this.captainProjectileSpeed = config.captainProjectileSpeed;
+        this.captainSpeed = config.captainSpeed;
+    }
+    /** @param {Phaser.Scene} scene */
+    static preload(scene) {
+        scene.load.spritesheet("captain-idle", "images/captain_idle_strip.png", {
+            frameWidth: 64,
+            frameHeight: 64,
+        });
+        scene.load.spritesheet("captain-run", "images/captain_run_strip.png", {
+            frameWidth: 64,
+            frameHeight: 64,
+        });
+        scene.load.spritesheet(
+            "captain-attack",
+            "images/captain_attack_strip.png",
+            {
+                frameWidth: 64,
+                frameHeight: 64,
+            }
+        );
+        scene.load.spritesheet("poison-ball", "images/poisonBall_strip.png", {
+            frameWidth: 36,
+            frameHeight: 36,
+        });
+        scene.load.spritesheet(
+            "poison-ball-explosion",
+            "images/poisonBall_explosion_strip.png",
+            {
+                frameWidth: 96,
+                frameHeight: 96,
+            }
+        );
+        scene.load.audio("captain-dies", "audio/captain-dies.mp3");
+        scene.load.audio("captain-roar", "audio/captain-roar.mp3");
+        scene.load.audio("enemy-damaged", "audio/enemy-damaged.mp3");
+        scene.load.audio("enemy-hit-aah", "audio/enemy-hit-aah.mp3");
+    }
+    create() {
+        super.create();
+        this.captain = this.mainSprite = this.scene.add.sprite(250, 500);
+        this.captain.setScale(PIXEL_SCALE);
+        this.captain.play("captain-run");
+        this.scene.physics.add.existing(this.captain);
+        this.captain.setDataEnabled();
+        this.captain.data.set("actor", this);
 
-    this.poisonBalls = this.scene.add.group({
-      classType: PoisonBall,
-      maxSize: 50,
-      runChildUpdate: true,
-    });
+        this.poisonBalls = this.scene.add.group({
+            classType: PoisonBall,
+            maxSize: 50,
+            runChildUpdate: true,
+            // link the ball to the captian who fired it so that captain can be excluded from collisions
+            createCallback: ball => ball.originator = this,
+        });
 
-    // save a reference to the captain body with the correct type
-    /** @type {Phaser.Physics.Arcade.Body} */
-    this.captainBody = this.captain.body;
-    this.captainBody.immovable = true;
+        // save a reference to the captain body with the correct type
+        /** @type {Phaser.Physics.Arcade.Body} */
+        this.captainBody = this.captain.body;
+        this.captainBody.immovable = true;
 
-    // adjust hitbox
-    this.captainBody.setSize(28, 26);
-    this.captainBody.setOffset(17, 28);
+        // adjust hitbox
+        this.captainBody.setSize(28, 26);
+        this.captainBody.setOffset(17, 28);
 
-    this.isAttacking = false;
-  }
-
-  update() {
-    this.captain.depth = this.captain.y + this.captain.height;
-
-    this.playerDistance = new Phaser.Math.Vector2()
-      .copy(this.scene.player.player)
-      .subtract(this.captain);
-
-    if (this.playerDistance.length() < this.captainAttackRange) {
-      this.attack();
+        this.isAttacking = false;
     }
 
-    this.handleMovement();
-  }
+    update() {
+        this.captain.depth = this.captain.y + this.captain.height;
 
-  /**
-   * Create animations to be used by any Captain instances.
-   * @param {Phaser.Scene} scene
-   */
-  static createAnims(scene) {
-    // loop through each spritesheet and create an animation
-    ["captain-idle", "captain-run", "poison-ball"].forEach((name) => {
-      scene.anims.create({
-        key: name,
-        frames: scene.anims.generateFrameNumbers(name),
-        frameRate: 10,
-        repeat: -1,
-      });
-    });
-    ["captain-attack", "poison-ball-explosion"].forEach((name) => {
-      let res = scene.anims.create({
-        key: name,
-        frames: scene.anims.generateFrameNumbers(name),
-        frameRate: 10,
-        repeat: 0,
-      });
-    });
-  }
+        this.playerDistance = new Phaser.Math.Vector2()
+            .copy(this.scene.player.player)
+            .subtract(this.captain);
 
-  handleMovement() {
-    if (!this.isAttacking) {
-      const targetX = this.scene.player.player.x;
-      this.captain.setFlipX(targetX > this.captain.x);
-      const vel = this.playerDistance.normalize().scale(this.captainSpeed);
-      this.captainBody.setVelocity(vel.x, vel.y);
+        if (this.playerDistance.length() < this.captainAttackRange) {
+            this.attack();
+        }
+
+        this.handleMovement();
     }
-  }
 
-  /** Attack, if we're in a state that allows attacking. */
-  attack() {
-    if (this.isAttacking || !this.playerDistance) return;
-
-    const targetX = this.scene.player.player.x;
-    this.captain.setFlipX(targetX > this.captain.x);
-
-    this.isAttacking = true;
-
-    this.captainBody.setVelocity(0, 0);
-
-    const poisonOffset = new Phaser.Math.Vector2(20, -50);
-    if (this.captain.flipX) {
-      poisonOffset.x *= -1;
+    /**
+     * Create animations to be used by any Captain instances.
+     * @param {Phaser.Scene} scene
+     */
+    static createAnims(scene) {
+        // loop through each spritesheet and create an animation
+        ["captain-idle", "captain-run", "poison-ball"].forEach((name) => {
+            scene.anims.create({
+                key: name,
+                frames: scene.anims.generateFrameNumbers(name),
+                frameRate: 10,
+                repeat: -1,
+            });
+        });
+        ["captain-attack", "poison-ball-explosion"].forEach((name) => {
+            let res = scene.anims.create({
+                key: name,
+                frames: scene.anims.generateFrameNumbers(name),
+                frameRate: 10,
+                repeat: 0,
+            });
+        });
     }
-    const poisonPos = new Phaser.Math.Vector2()
-      .copy(this.captain)
-      .subtract(poisonOffset);
 
-    let poisonBallDirection = this.playerDistance;
-
-    this.scene.time.addEvent({
-      delay: 600,
-      callback: () => this.spawnPoisonBalls(poisonPos, poisonBallDirection),
-      callbackScope: this,
-    });
-
-    this.captain.anims.stop();
-    this.captain.play("captain-attack");
-
-    this.captain.on(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
-      this.captain.play("captain-idle");
-      this.scene.time.addEvent({
-        delay: this.captainIdleAfterAttack,
-        callback: this.attackComplete,
-        callbackScope: this,
-      });
-    });
-  }
-
-  attackComplete() {
-    if (!this.isAlive) return;
-
-    this.captain.play("captain-run");
-    this.isAttacking = false;
-  }
-
-  spawnPoisonBalls(pos, dir) {
-    let patternChance = Phaser.Math.RND.frac() * 100;
-    if (patternChance < 50) {
-      this.linearSpawnPattern(pos, dir);
-    } else {
-      this.arcSpawnPattern(pos, dir);
+    handleMovement() {
+        if (!this.isAttacking) {
+            const targetX = this.scene.player.player.x;
+            this.captain.setFlipX(targetX > this.captain.x);
+            const vel = this.playerDistance.normalize().scale(this.captainSpeed);
+            this.captainBody.setVelocity(vel.x, vel.y);
+        }
     }
-  }
 
-  linearSpawnPattern(pos, dir) {
-    this.scene.time.addEvent({
-      delay: 50,
-      callback: () => this.spawnPoisonBall(pos, dir),
-      callbackScope: this,
-      repeat: 4,
-    });
-  }
+    /** Attack, if we're in a state that allows attacking. */
+    attack() {
+        if (this.isAttacking || !this.playerDistance) return;
 
-  arcSpawnPattern(pos, dir) {
-    const spreadAngle = Phaser.Math.DegToRad(10);
-    let offsetAngle = -1.5 * spreadAngle;
-    this.scene.time.addEvent({
-      delay: 50,
-      callback: () => {
-        this.spawnPoisonBall(pos, dir.rotate(offsetAngle));
-        offsetAngle += spreadAngle;
-      },
-      callbackScope: this,
-      repeat: 4,
-    });
-  }
+        const targetX = this.scene.player.player.x;
+        this.captain.setFlipX(targetX > this.captain.x);
 
-  spawnPoisonBall(pos, dir) {
-    let poisonBall = this.poisonBalls.get();
-    if (poisonBall) {
-      poisonBall.fire(pos.x, pos.y, dir);
+        this.isAttacking = true;
+
+        this.captainBody.setVelocity(0, 0);
+
+        const poisonOffset = new Phaser.Math.Vector2(20, -50);
+        if (this.captain.flipX) {
+            poisonOffset.x *= -1;
+        }
+        const poisonPos = new Phaser.Math.Vector2()
+            .copy(this.captain)
+            .subtract(poisonOffset);
+
+        let poisonBallDirection = this.playerDistance;
+
+        this.scene.time.addEvent({
+            delay: 600,
+            callback: () => this.spawnPoisonBalls(poisonPos, poisonBallDirection),
+            callbackScope: this,
+        });
+
+        this.captain.anims.stop();
+        this.captain.play("captain-attack");
+
+        this.captain.on(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+            this.captain.play("captain-idle");
+            this.scene.time.addEvent({
+                delay: this.captainIdleAfterAttack,
+                callback: this.attackComplete,
+                callbackScope: this,
+            });
+        });
     }
-  }
 
-  /**
-   * Cause damage to this actor.
-   * @param {number} inflictedDamage
-   */
-  takeDamage(inflictedDamage) {
-    super.takeDamage(inflictedDamage);
-    if (this.vulnerable) {
-      this.scene.sound.play(
-        ["enemy-damaged", "enemy-hit-aah"][Math.round(Math.random())]
-      );
+    attackComplete() {
+        if (!this.isAlive) return;
+
+        this.captain.play("captain-run");
+        this.isAttacking = false;
     }
-  }
 
-  playDeathAnim() {
-    super.playDeathAnim();
-    this.captain.removeAllListeners();
-    this.captain.anims.stop();
-    // this seems like a good death anim, with the hands flying up
-    this.scene.sound.play("captain-dies", { volume: 4 });
-  }
+    spawnPoisonBalls(pos, dir) {
+        let patternChance = Phaser.Math.RND.frac() * 100;
+        if (patternChance < 50) {
+            this.linearSpawnPattern(pos, dir);
+        } else {
+            this.arcSpawnPattern(pos, dir);
+        }
+    }
 
-  dealDamage() {
-    this.scene.sound.play(["axe-hit1", "axe-hit2"][Math.round(Math.random())]);
-  }
+    linearSpawnPattern(pos, dir) {
+        this.scene.time.addEvent({
+            delay: 50,
+            callback: () => this.spawnPoisonBall(pos, dir),
+            callbackScope: this,
+            repeat: 4,
+        });
+    }
+
+    arcSpawnPattern(pos, dir) {
+        const spreadAngle = Phaser.Math.DegToRad(10);
+        let offsetAngle = -1.5 * spreadAngle;
+        this.scene.time.addEvent({
+            delay: 50,
+            callback: () => {
+                this.spawnPoisonBall(pos, dir.rotate(offsetAngle));
+                offsetAngle += spreadAngle;
+            },
+            callbackScope: this,
+            repeat: 4,
+        });
+    }
+
+    spawnPoisonBall(pos, dir) {
+        let poisonBall = this.poisonBalls.get();
+        if (poisonBall) {
+            poisonBall.fire(pos.x, pos.y, dir);
+        }
+    }
+
+    /**
+     * Cause damage to this actor.
+     * @param {number} inflictedDamage
+     */
+    takeDamage(inflictedDamage) {
+        super.takeDamage(inflictedDamage);
+        if (this.vulnerable) {
+            this.scene.sound.play(
+                ["enemy-damaged", "enemy-hit-aah"][Math.round(Math.random())]
+            );
+        }
+    }
+
+    playDeathAnim() {
+        super.playDeathAnim();
+        this.captain.removeAllListeners();
+        this.captain.anims.stop();
+        // this seems like a good death anim, with the hands flying up
+        this.scene.sound.play("captain-dies", { volume: 4 });
+    }
+
+    dealDamage() {
+        this.scene.sound.play(["axe-hit1", "axe-hit2"][Math.round(Math.random())]);
+    }
 }
--- actors/poison_ball.js
+++ actors/poison_ball.js
@@ -1,72 +1,78 @@
 import Phaser from "phaser";
 import {
-  CAPTAIN_ATTACK_DAMAGE,
-  CAPTAIN_PROJECTILE_SPEED,
-  PIXEL_SCALE,
+    CAPTAIN_ATTACK_DAMAGE,
+    CAPTAIN_PROJECTILE_SPEED,
+    PIXEL_SCALE,
 } from "../variables";
 
 export class PoisonBall extends Phaser.GameObjects.Sprite {
-  /** @param {Phaser.Scene} scene */
-  constructor(scene) {
-    super(scene, 0, 0, "poison-ball");
+    /** @param {Phaser.Scene} scene */
+    constructor(scene) {
+        super(scene, 0, 0, "poison-ball");
 
-    scene.physics.add.existing(this);
-    this.setScale(PIXEL_SCALE);
+        scene.physics.add.existing(this);
+        this.setScale(PIXEL_SCALE);
 
-    this.setDataEnabled(true);
-    this.data.set("actor", this);
+        this.setDataEnabled(true);
+        this.data.set("actor", this);
 
-    scene.physics.add.overlap(
-      this,
-      scene.player.player,
-      (projectile, player, colInfo) => {
-        if (this.active) {
-          let poisonActor = projectile.data.get("actor");
-          let playerActor = player.data.get("actor");
-          playerActor.takeDamage(poisonActor.damage);
-          this.spawnExplosion();
-          this.die();
-        }
-      },
-      // collide only if the player is vulnerable
-      () => scene.player.vulnerable
-    );
 
-    // adjust hitbox
-    this.body.setSize(16, 16);
-    this.depth = 10000;
+        // adjust hitbox
+        this.body.setSize(16, 16);
+        this.depth = 10000;
 
-    this.damage = CAPTAIN_ATTACK_DAMAGE;
-  }
+        this.damage = CAPTAIN_ATTACK_DAMAGE;
+    }
 
-  fire(x, y, direction) {
-    let vel = direction.normalize().scale(CAPTAIN_PROJECTILE_SPEED);
-    this.body.setVelocity(vel.x, vel.y);
-    this.play("poison-ball");
-    this.setPosition(x, y - 50);
+    fire(x, y, direction) {
 
-    this.setActive(true);
-    this.setVisible(true);
+        this.scene.physics.add.overlap(
+            this,
+            [
+                this.scene.player.player,
+                ...this.scene.pinkies.map((pinky) => pinky.pinky),
+                ...this.scene.captains.filter((captain) => captain != this.originator).map((captain) => captain.captain),
+            ],
+            (projectile, player, colInfo) => {
+                if (this.active) {
+                    let poisonActor = projectile.data.get("actor");
+                    let actor = player.data.get("actor");
+                    actor.takeDamage(poisonActor.damage);
+                    this.spawnExplosion();
+                    this.die();
+                }
+            },
+            // collide only if the player is vulnerable
+            () => this.scene.player.vulnerable
+        );
 
-    this.scene.time.addEvent({
-      delay: 10000,
-      callback: this.die,
-      callbackScope: this,
-    });
-  }
+        let vel = direction.normalize().scale(CAPTAIN_PROJECTILE_SPEED);
+        this.body.setVelocity(vel.x, vel.y);
+        this.play("poison-ball");
+        this.setPosition(x, y - 50);
 
-  spawnExplosion() {
-    let explosion = this.scene.add.sprite(this.x, this.y);
-    explosion.setScale(PIXEL_SCALE);
-    // put explosion on top of everything (probably)
-    explosion.setDepth(5000);
-    explosion.play("poison-ball-explosion");
-    explosion.on(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
-      explosion.destroy();
-    });
-  }
+        this.setActive(true);
+        this.setVisible(true);
 
-  die() {
-    this.destroy();
-  }
+        this.scene.time.addEvent({
+            delay: 10000,
+            callback: this.die,
+            callbackScope: this,
+        });
+    }
+
+    spawnExplosion() {
+        let explosion = this.scene.add.sprite(this.x, this.y);
+        explosion.setScale(PIXEL_SCALE);
+        // put explosion on top of everything (probably)
+        explosion.setDepth(5000);
+        explosion.play("poison-ball-explosion");
+        explosion.on(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+            explosion.destroy();
+        });
+    }
+
+    die() {
+        this.destroy();
+    }
 }
